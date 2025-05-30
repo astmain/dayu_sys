b4da8143b928310132071a37fd9f6e3acbbccb20
xupeng <1311192345@qq.com>
2025-05-30 20:03:13 +0800

    模版代码

--- 模版代码/invoice_infos_Module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { invoice_infos } from './invoice_infos';
-
-@Module({
-  controllers: [invoice_infos],
-  providers: [],
-})
-export class invoice_infos_Module {}
