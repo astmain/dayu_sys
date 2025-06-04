459be3c6490042380c094d81e139198a1242e2e6
hackerconding <85149175+hackerconding@users.noreply.github.com>
2025-06-05 00:06:35 +0800

    filter_error_sys

--- dayu_sys02/test01/new_expressage__2025-06-04-16-43-43/expressage/dto/expressage_create.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsArray, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Length, Min } from "class-validator";
-export class expressage_create {
-    @ApiProperty({ description: '默认名称', type: String, example: "默认名称" })
-    @IsString()
-    @IsNotEmpty()
-    name: string = "111";
-}
