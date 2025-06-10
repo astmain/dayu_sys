c7c472f5b722818aebcc4885759ed3bb8b663d48
hackerconding <85149175+hackerconding@users.noreply.github.com>
2025-06-11 01:40:27 +0800

    demo1

--- dayu_sys02/demo1/src/app.module.ts
+++ dayu_sys02/demo1/src/app.module.ts
@@ -1,7 +1,6 @@
 import {Module} from '@nestjs/common';
 
 // 引入控制器
-import {orm1_module} from '@Controller/orm1/orm1';
 import {orm1} from '@Controller/orm1/orm1';
 import {test1_module} from '@Controller/test1/test1_module';
 // 引入数据库
